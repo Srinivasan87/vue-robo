4c764941d15a4e4ebeaa6b857f7ac9b70f3b2af4
Srinivasan Jayaraman <45791344+Srinivasan87@users.noreply.github.com>
2019-06-05 03:42:59 -0400

    Add props to pass data to child componnets
    Add props to pass data to child componnets

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -6,15 +6,15 @@
           {{ this.selectedRobot.head }}
           <span v-show="selectedRobot.Head.onSale" class="sale">Sale!</span>
         </div> -->
-        <PartSelector/>
+        <PartSelector :parts="availableParts.heads" position="top"/>
     </div>
     <div class="middle-row">
-      <PartSelector/>
-      <PartSelector/>
-      <PartSelector/>
+      <PartSelector :parts="availableParts.arms" position="left"/>
+      <PartSelector :parts="availableParts.torsos" position="center"/>
+      <PartSelector :parts="availableParts.arms" position="right"/>
     </div>
     <div class="bottom-row">
-      <PartSelector/>
+      <PartSelector :parts="availableParts.bases" position="bottom"/>
     </div>
     <div>
       <h1>Cart</h1>
